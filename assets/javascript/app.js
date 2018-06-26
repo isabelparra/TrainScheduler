b1ac57db178f7849c55bbba3a6b62fc95de148a0
Isabel Parra <isabelparra@Isabels-MacBook-Pro.local>
2018-06-26 12:37:00 -0400

    child added function and change html to reflect user data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAgKHXlalIyAq1p9F9rI1aDIHega8Sk_cc",
-    authDomain: "train-scheduler-f73e1.firebaseapp.com",
-    databaseURL: "https://train-scheduler-f73e1.firebaseio.com",
-    projectId: "train-scheduler-f73e1",
-    storageBucket: "train-scheduler-f73e1.appspot.com",
-    messagingSenderId: "216142055388"
-  };
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAPEKcEoEBV7HxvJt87pl0R_Th5_JrD9zs",
+    authDomain: "train-d10be.firebaseapp.com",
+    databaseURL: "https://train-d10be.firebaseio.com",
+    projectId: "train-d10be",
+    storageBucket: "",
+    messagingSenderId: "841280019518"
+  };
   firebase.initializeApp(config);
 
   var database = firebase.database();
@@ -15,7 +15,7 @@
   var tName = '';
   var tDestination = '';
   var firstTime = '03:30';
-  var tFrequency = '';
+  var tFrequency = 0;
 
 // Button for adding trains
 $('#add-train').on('click', function(event) {
@@ -23,54 +23,23 @@ $('#add-train').on('click', function(event) {
     event.preventDefault();
 
     // Grabs user input
-    var tName = $('#train-name-input').val().trim();
+    var tName = $('#name-input').val().trim();
     var tDestination = $('#destination-input').val().trim();
-    var firstTime = moment($('#first-train-input').val().trim(), 'DD/MM/YY').format('X');
+    var firstTime = moment($('#start-input').val().trim(), 'DD/MM/YY').format('X');
     var tFrequency = $('#frequency-input').val().trim();
     
    
     // creates local 'temp' object for holding data
 
-    database.ref().set({
+    database.ref().push({
         name: tName,
         destination: tDestination, 
         start: firstTime, 
         frequency: tFrequency
     });
-    return false;
 });
 
-// print changes to the console and html
-    database.ref().on('value', function(snapshot) {
-        console.log(snapshot.val());
-
-        console.log(snapshot.val().tName);
-        console.log(snapshot.val().tDestination);
-        console.log(snapshot.val().firstTime);
-        console.log(snapshot.val().tFrequency);
-
-        $('#train-table > tbody').append('<tr><td>' + tName + '</td><td>' + tDestination + '</td><td>' + firstTime + '</td><td>' + tFrequency + '</td></tr>');
-        
-    }, function(errorObject) {
-        console.log('the read failed: ' + console.errorObject.code);
-    }); 
-   
-
-
-
-
-
-
     // uploads train data to the database
-
-//     database.ref().push(newTrain);
-
-//     // logs everything to console
-
-//     console.log(newTrain.name);
-//     console.log(newTrain.destination);
-//     console.log(newTrain.start);
-//     console.log(newTrain.frequency);
 
 //     // alert 
 
@@ -84,13 +53,30 @@ $('#add-train').on('click', function(event) {
 
 // });
 
-// // 3. create firebase event for adding employee to the database and a row in the html when user adds entry
+// // 3. create firebase event for adding train to the database and a row in the html when user adds entry
 
-// database.ref().on('child_added', function(childSnapshot, prevChildKey) {
+database.ref().on('child_added', function(snapshot) {
+    var newTrain = snapshot.val();
+
+//     database.ref().push(newTrain);
+
+//     // logs everything to console
+
+console.log(newTrain.name);
+console.log(newTrain.destination);
+console.log(newTrain.start);
+console.log(newTrain.frequency);
+
+$('#train-table > tbody').append('<tr><td>' + tName + '</td><td>' + tDestination + '</td><td>' + firstTime + '</td><td>' + tFrequency + '</td></tr>');
+      
+    }, function(errorObject) {
+        console.log('Errors handled: ' + errorObject.code);
+    });      
     
-//     console.log(childSnapshot.val());
+    
+// console.log(childSnapshot.val());
 
-//     // store everything into a variable
+// //     // store everything into a variable
 //     var tName = childSnapshot.val().name;
 //     var tDestination = childSnapshot.val().destination;
 //     var firstTime = childSnapshot.val().start;
@@ -101,6 +87,35 @@ $('#add-train').on('click', function(event) {
 //     console.log(tDestination);
 //     console.log(firstTime);
 //     console.log(tFrequency);
+
+
+
+
+
+
+
+
+
+
+
+// // print changes to the console and html
+//     database.ref().on('value', function(snapshot) {
+//         console.log(snapshot.val());
+
+//         console.log(snapshot.val().tName);
+//         console.log(snapshot.val().tDestination);
+//         console.log(snapshot.val().firstTime);
+//         console.log(snapshot.val().tFrequency);
+
+//     
+   
+
+
+
+
+
+
+
 
     // prettify the employee start
 
